715eabf554739db5e1019b0f2fb6c2b358d30d9d
TINODIWA <130966831+TINODIWA@users.noreply.github.com>
2025-04-23 01:22:30 +0200

    6th-commit Fixed ReportTab Again

---
+++ NotificationsTab.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.u24825532u24580482.assignment4;
+
+import javax.swing.*;
+import java.awt.*;
+import java.sql.*;
+/**
+ *
+ * @author natha
+ */
+public class NotificationsTab {
+    
+}
