8e78c71bced2348552a3ae54f5603b248a4255fc
Domy05 <u24580482@tuks.co.za>
2025-04-22 20:40:45 +0200

    second commit

---
+++ ReportTab.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.u24825532u24580482.assignment4;
+
+import javax.swing.*;
+import java.awt.*;
+import java.sql.*;
+/**
+ *
+ * @author natha
+ */
+public class ReportTab extends JPanel {
+//    private final JLabel totalProductsLabel;
+    public ReportTab(){
+        
+    }
+}
